ce3e05c9e93b82d070eead085b784814b5e5c58d
Thalles Rodrigues <tcarreiro.rodrigues@gmail.com>
2024-07-29 22:32:44 -0300

    FIX: fix code to coordinators and translate comments

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 import cv2
 from ultralytics import YOLO
 
-# Carregue o modelo YOLOv8
-model = YOLO('yolov8n.pt')  # Use 'yolov8n.pt' para o modelo nano, ou ajuste conforme necessário
+# Load YOLOv8 model
+# yolov8n.pt (nano), 
+# yolov8s.pt (small), 
+# yolov8m.pt (medium), 
+# yolov8l.pt (large) or 
+# yolov8x.pt (extra-large).
+model = YOLO('yolov8m.pt')
 
-# Inicialize a câmera
-cap = cv2.VideoCapture(0)  # Use '0' para a webcam integrada, ou ajuste conforme necessário
+
+# Cam init
+cap = cv2.VideoCapture(0)  # Use '0' to integrated webcam, or adjust
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Realize a detecção de objetos
-    results = model(frame)
+    # Predict Objects
+    results = model(frame, conf_thres=0.5, iou_thres=0.4)
 
-    # Desenhe as caixas delimitadoras e etiquetas no frame
-    for result in results:
-        for box in result.boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy)
-            label = box.cls_name
-            confidence = box.conf
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, f'{label} {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # Extract Detections
+    detections = results[0]
+    
+    # Draw bounding box and label
+    for box in detections.boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0])  # Coordinator
+        label = model.names[int(box.cls[0])]    # Classe Name
+        confidence = box.conf[0].item()         # Confiance
+        
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(frame, f'{label} {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    # Mostre o frame com as detecções
+    # Show frame with detections
     cv2.imshow('YOLOv8 Object Detection', frame)
 
-    # Saia do loop ao pressionar a tecla 'q'
+    # Press key 'q' to end loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Libere a câmera e feche as janelas
+# Release Cam and close windows
 cap.release()
 cv2.destroyAllWindows()
